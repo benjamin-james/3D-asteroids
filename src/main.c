23bbff410c8b696b5dca18cb550b1ba3d939ed6a
pbandj24 <2015376@sluh.org>
2015-02-19 09:17:48 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,5 +3,25 @@
 
 int main(int argc, char **argv)
 {
-  
+  int width = 800;
+  int height = 600;
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Window *window = SDL_CreateWindow(*argv, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);	
+  SDL_GL_CreateContext(window);
+	Display_InitGL();
+	Display_SetViewport(WINDOW_WIDTH, WINDOW_HEIGHT);
+  int running = 1;
+	while(running)
+	{
+	  SDL_Event e;
+	  while(SDL_PollEvent(&e))
+	  {
+	    if(e.type == SDL_QUIT) running = 0;
+	    handleEvent(&e);
+	  }
+	  Display_Render(window);
+	}
+	SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }

47f6d3872711fcd5c654414b8bbba6509df6b255
pbandj24 <2015376@sluh.org>
2015-02-19 16:51:13 -0600

    Update game.c

--- src/game.c
+++ src/game.c
@@ -1,11 +1,27 @@
 #include "game.h"
+#include "SDL2/SDL.h"
 #include "SDL2/SDL_opengl.h"
 
 void render()
 {
 
 }
-void update(double float)
+void update(double delta)
 {
 
+}
+void handleKey(SDL_Keycode key, Uint32 status)
+{
+	if(status == SDL_PRESSED)
+	{
+	  //do stuff
+	}
+	else if(status == SDL_RELEASED)
+	{
+	  //do stuff
+	}
+}
+void mouseMoved(int x, int y) //delta position
+{
+  
 }

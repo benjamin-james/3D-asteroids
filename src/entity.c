8791fd46b93c760425a141bd24ea66b350367d6d
pbandj24 <2015376@sluh.org>
2015-02-20 10:18:54 -0600

    Create entity.c

---
+++ src/entity.c
@@ -0,0 +1,29 @@
+#include "entity.h"
+#include "matrix.h"
+
+#include "SDL2/SDL_opengl.h"
+
+entity entity_create()
+{
+  entity e = malloc(sizeof(entity_t));
+  e->pos = _vec3(0,0,0);
+  e->rot = _vec3(0,0,0);
+  e->vel = _vec3(0,0,0);
+  e->index = 0;
+  return e;
+}
+void entity_destroy(entity e)
+{
+  if(!e) return;
+  if(e->index) glDeleteLists(e->index,1);
+  free(e);
+}
+
+void useView(entity e)
+{
+  
+}
+void render(entity e)
+{
+  
+}

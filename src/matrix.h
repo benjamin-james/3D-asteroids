8d238c24a0f2cb3cb2381aefab86e4dba3e47469
pbandj24 <2015376@sluh.org>
2015-02-20 09:03:48 -0600

    Update matrix.h

--- src/matrix.h
+++ src/matrix.h
@@ -13,12 +13,10 @@ typedef struct
 
 mat4 _mat4();
 mat4 mat4_multiply(mat4 m1, mat4 m2);
-vec4 _vec4(GLfloat a, GLfloat b, GLfloat c, GLfloat d);*/
 vec3 _vec3(float a, float b, float c);
 mat4 mat4_clone(mat4 m);
 mat4 mat4_translate(mat4 m, vec3 pos);
 mat4 mat4_rotate(mat4 m, vec3 rot);
-mat4 _mat4_rotate(vec3 rot);
 vec3 vec3_rot(vec3 pos, vec3 rot);
 vec3 rotz(vec3 v, float a);
 vec3 roty(vec3 v, float a);

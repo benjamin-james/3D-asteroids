1f837c8adf24b6ab188397959072f29616abcf79
pbandj24 <2015376@sluh.org>
2015-02-19 19:14:08 -0600

    Update game.c

--- src/game.c
+++ src/game.c
@@ -4,13 +4,41 @@
 
 double joystick[2]; //joystick angle and magnitude
 
+double x,y,z,rx,ry,rz;
+double speed = 2.0;
 void render()
 {
-
+	glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+	glLoadIdentity();
+	glRotatef(rx,1,0,0);
+	glRotatef(ry,0,1,0);
+	glRotatef(rz,0,0,1);
+	glTranslatef(x,y,z);
+	
+	glBegin(GL_TRIANGLES)
+	{
+		glColor4f(1,0,0,1);
+		glVertex3f(0,0,1);
+		glVertex3f(0.5,0,-0.5);
+		glVertex3f(0,0,-0.2);
+		
+		glVertex3f(0,0,-0.2);
+		glVertex3f(-0.5,0,-0.5);
+		glVertex3f(0,0,1);
+		
+		glColor4f(0,0,1,1);
+		glVertex3f(0,0,1);
+		glVertex3f(0,0,-0.2);
+		glVertex3f(0,0.5,-0.5);
+	}
+	glEnd();
 }
 void update(double delta)
 {
-
+	ry += delta*joystick[1];
+	rz += delta*joystick[0];
+	z += delta*speed;
 }
 void handleKey(SDL_Keycode key, Uint32 status)
 {
@@ -25,6 +53,8 @@ void handleKey(SDL_Keycode key, Uint32 status)
 }
 void joystick(double x, double y) //range of [-1,1] for x and y
 {
-	joystick[0] = atan2(y,x);
-	joystick[1] = hypot(x,y);
+	joystick[0] = x;
+	joystick[1] = y;
+	//joystick[0] = atan2(y,x);
+	//joystick[1] = hypot(x,y);
 }

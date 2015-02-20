a5c178e800aa25c6bc34d46976a9264e17779fee
pbandj24 <2015376@sluh.org>
2015-02-19 18:59:27 -0600

    Update matrix.c

--- src/matrix.c
+++ src/matrix.c
@@ -1,6 +1,14 @@
 #include "matrix.h"
 #include "GL/gl.h"
 
+vec3 rotx(vec3 v, GLdouble a)
+{
+  return _vec(v[0],v[1]*cos(a) - v[2]*sin(a),v[1]*sin(a) + v[2]*cos(a));
+}
+vec3 vec3_rot(vec3 v, vec3 axis, GLdouble a)
+{
+  
+}
 inline void m_set(mat4 m, int x, int y, GLdouble value)
 {
   m[x*4+y] = value;
@@ -39,7 +47,7 @@ mat4 mat4_clone(mat4 m)
 }
 mat4 mat4_translate(mat4 m, vec3 axis)
 {
-  
+  return _mat4();//TODO: do this
 }
 mat4 _mat4_translate(vec3 axis)
 {
@@ -47,7 +55,7 @@ mat4 _mat4_translate(vec3 axis)
 }
 mat4 mat4_rotate(mat4 m, vec3 axis)
 {
-  
+  return _mat4();//TODO: do this
 }
 mat4 _mat4_rotate(vec3 axis)
 {

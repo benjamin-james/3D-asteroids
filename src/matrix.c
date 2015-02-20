210af0d9fc0fdd16c336cf91effc6420488fa4fd
pbandj24 <2015376@sluh.org>
2015-02-19 20:17:56 -0600

    Update matrix.c

--- src/matrix.c
+++ src/matrix.c
@@ -3,22 +3,24 @@
 
 vec3 rotx(vec3 v, GLfloat a)
 {
-  return _vec3(v[0],v[1]*cos(a) - v[2]*sin(a),v[1]*sin(a) + v[2]*cos(a));
+  return _vec3(v.x,v.y*cos(a) - v.z*sin(a),v.y*sin(a) + v.z*cos(a));
 }
 vec3 roty(vec3 v, GLfloat a)
 {
-  return _vec3(v[0]*cos(a)+v[2]*sin(a),v[1],-v[0]*sin(a)+v[2]*cos(a));
+  return _vec3(v.x*cos(a)+v.z*sin(a),v.y,-v.x*sin(a)+v.z*cos(a));
 }
 vec3 rotz(vec3 v, GLfloat a)
 {
-  return _vec3(v[0]*cos(a) - v[1]*sin(a),v[0]*sin(a)+v[1]*cos(a),v[2]);
+  return _vec3(v.x*cos(a) - v.y*sin(a),v.x*sin(a)+v.y*cos(a),v.z);
 }
 vec3 vec3_rot(vec3 pos, vec3 rot)
 {
-  return _vec3( pos[0]*rotz(roty(rotx(_vec3(1.f,0.f,0.f),rot[0]),rot[1]),rot[2]),
-                pos[1]*rotz(roty(rotx(_vec3(0.f,1.f,0.f),rot[0]),rot[1]),rot[2]),
-                pos[2]*rotz(roty(rotx(_vec3(0.f,0.f,1.f),rot[0]),rot[1]),rot[2]));
-}
+  return _vec3( 
+                pos.x*rotz(roty(rotx(_vec3(1.f,0.f,0.f),rot.x),rot.y),rot.y),
+                pos.y*rotz(roty(rotx(_vec3(0.f,1.f,0.f),rot.x),rot.y),rot.y),
+                pos.z*rotz(roty(rotx(_vec3(0.f,0.f,1.f),rot.x),rot.y),rot.y)
+              );
+}/*
 inline void m_set(mat4 m, int x, int y, GLfloat value)
 {
   m[x*4+y] = value;
@@ -39,12 +41,12 @@ vec4 _vec4(GLfloat a, GLfloat b, GLfloat c, GLfloat d)
 {
   vec4 v = {a,b,c,d};
   return v;
-}
+}*/
 vec3 _vec3(GLfloat a, GLfloat b, GLfloat c)
 {
   vec3 v = {a,b,c};
   return v;
-}
+}/*
 mat4 mat4_clone(mat4 m)
 {
   int i;
@@ -70,4 +72,4 @@ mat4 mat4_rotate(mat4 m, vec3 axis)
 mat4 _mat4_rotate(vec3 axis)
 {
   return mat4_rotate(_mat4(),axis);
-}
+}*/

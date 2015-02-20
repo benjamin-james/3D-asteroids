cbd7305c5b432f0d3bbe6f578d860e3dfc9bea49
pbandj24 <2015376@sluh.org>
2015-02-19 18:34:16 -0600

    Update matrix.c

--- src/matrix.c
+++ src/matrix.c
@@ -1,3 +1,9 @@
 #include "matrix.h"
 #include "GL/gl.h"
 
+vec4 _vec4(GLdouble a, GLdouble b, GLdouble c, GLdouble d);
+vec3 _vec3(GLdouble a, GLdouble b, GLdouble c);
+mat4 _mat4();
+mat4 mat4_clone(mat4 m);
+mat4 mat4_translate(mat4 m, vec3 axis);
+mat4 mat4_rotate(mat4 m, vec3 axis);

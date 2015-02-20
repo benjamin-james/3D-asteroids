dbfc3f9dffabb2037e2d8169959d15c655989ed2
pbandj24 <2015376@sluh.org>
2015-02-19 20:15:32 -0600

    Update matrix.h

--- src/matrix.h
+++ src/matrix.h
@@ -4,16 +4,18 @@
 #include "GL/gl.h" //for GLfloat
 
 typedef GLfloat mat4[16];
-typedef GLfloat vec3[3];
-typedef GLfloat vec4[4];
+typedef struct
+{
+  GLfloat x,y,z;
+} vec3;
 
-mat4 _mat4();
-vec4 _vec4(GLfloat a, GLfloat b, GLfloat c, GLfloat d);
+/*mat4 _mat4();
+vec4 _vec4(GLfloat a, GLfloat b, GLfloat c, GLfloat d);*/
 vec3 _vec3(GLfloat a, GLfloat b, GLfloat c);
-mat4 mat4_clone(mat4 m);
+/*mat4 mat4_clone(mat4 m);
 mat4 mat4_translate(mat4 m, vec3 pos);
 mat4 _mat4_translate(vec3 pos);
 mat4 mat4_rotate(mat4 m, vec3 rot);
-mat4 _mat4_rotate(vec3 rot);
+mat4 _mat4_rotate(vec3 rot);*/
 vec3 vec3_rot(vec3 pos, vec3 rot);
 #endif

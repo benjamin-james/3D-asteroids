df53c8f7d3cb402d413b36d8f47700c240b5ad22
pbandj24 <2015376@sluh.org>
2015-02-20 19:28:40 -0600

    Update game.c

--- src/game.c
+++ src/game.c
@@ -9,74 +9,57 @@ vec3 pos = {0,0,-6};
 vec3 rot = {0,0,0};
 
 GLfloat speed = 2.0;
+
+void renderShip()
+{
+	glRotatef(rot.x,1,0,0);
+	glRotatef(rot.y,0,1,0);
+	glRotatef(rot.z,0,0,1);
+	glTranslatef(-pos.x,-pos.y,-pos.z);
+	glBegin(GL_TRIANGLES);
+	{
+		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+		glVertex3f(0.0f, 0.0f, 0.0f);
+		glVertex3f(0.5f, 0.0f, -0.5f);
+		glVertex3f(0.0f, 0.0f, 2.0f);
+		
+		glVertex3f(0.0f, 0.0f, 0.0f);
+		glVertex3f(-0.5f, 0.0f, -0.5f);
+		glVertex3f(0.0f, 0.0f, 2.0f);
+
+		glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
+		glVertex3f(0.0f, 0.0f, 0.0f);
+		glVertex3f(0.0f, 1.0f, -0.5f);
+		glVertex3f(0.0f, 0.0f, 2.0f);
+	}
+	glEnd();
+}
 void render()
 {
 	glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
 	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 	glPushMatrix();
-	glRotatef(rot.x,1,0,0);
-	glRotatef(rot.y,0,1,0);
-	glRotatef(rot.z,0,0,1);
-	glTranslatef(pos.x,pos.y,pos.z);
-	
 	glBegin(GL_QUADS);
-
-		/* Cube Top */
-		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-		glVertex3f(-1.0f, 1.0f, 1.0f);
-		glVertex3f(-1.0f, 1.0f, -1.0f);
-		glVertex3f(1.0f, 1.0f, -1.0f);
-		glVertex3f(1.0f, 1.0f, 1.0f);
-
-
-		/* Cube Bottom */
-		glColor4f(1.0f, 0.5f, 0.0f, 1.0f);
-		glVertex3f(-1.0f, -1.0f, 1.0f);
-		glVertex3f(-1.0f, -1.0f, -1.0f);
-		glVertex3f(1.0f, -1.0f, -1.0f);
-		glVertex3f(1.0f, -1.0f, 1.0f);
-
-		/* Cube Front */
-		glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-		glVertex3f(-1.0f, 1.0f, 1.0f);
-		glVertex3f(1.0f, 1.0f, 1.0f);
-		glVertex3f(1.0f, -1.0f, 1.0f);
-		glVertex3f(-1.0f, -1.0f, 1.0f);
-
-		/* Cube Back */
-		glColor4f(0.0f, 1.0f, 0.5f, 1.0f);
-		glVertex3f(-1.0f, 1.0f, -1.0f);
-		glVertex3f(1.0f, 1.0f, -1.0f);
-		glVertex3f(1.0f, -1.0f, -1.0f);
-		glVertex3f(-1.0f, -1.0f, -1.0f);
-
-		/* Cube Left Side */
-		glColor4f(0.5f, 0.5f, 0.5f, 1.0f);
-		glVertex3f(-1.0f, 1.0f, -1.0f);
-		glVertex3f(-1.0f, 1.0f, 1.0f);
-		glVertex3f(-1.0f, -1.0f, 1.0f);
-		glVertex3f(-1.0f, -1.0f, -1.0f);
-
-		/* Cube Right Side */
-		glColor4f(0.15f, 0.25f, 0.75f, 1.0f);
-		glVertex3f(1.0f, 1.0f, -1.0f);
-		glVertex3f(1.0f, 1.0f, 1.0f);
-		glVertex3f(1.0f, -1.0f, 1.0f);
-		glVertex3f(1.0f, -1.0f, -1.0f);
-	
+		glColor4f(0.15f,0.9f,0.3f,1.0f);
+		glVertex3f(-10.0f,-1.0f,10.0f);
+		glVertex3f(-10.0f,-1.0f,-10.0f);
+		glVertex3f(10.0f,-1.0f,-10.0f);
+		glVertex3f(10.0f,-1.0f,10.0f);
 	glEnd();
+	renderShip();
 	glPopMatrix();
 	
 	glLoadIdentity();
 }
 void update(double delta)
 {
-	rot.x += 10*delta*stick.y;
-	rot.z += 10*delta*stick.x;
+	rot.y += 10*delta*stick.y;
+	rot.x += 10*delta*stick.x;
 	speed += delta*stick.z;
 	if(speed)//used for movement
 	{
-		vec3 dVec = vec3_rot(_vec3(0.f,0.f,speed*delta),rot);
+		//vec3 dVec = vec3_rot(_vec3(0.f,0.f,speed*delta),rot);
+		vec3 dVec = _vec3(speed*delta*sinf(rot.x)*cosf(rot.y),speed*delta*sinf(rot.x)*sinf(rot.y),speed*delta*cosf(rot.x));		
 		pos.x += dVec.x;
 		pos.y += dVec.y;
 		pos.z += dVec.z;

079ad4f6060f09c63d5f88203ec4293aaf463427
pbandj24 <2015376@sluh.org>
2015-02-20 20:42:47 -0600

    Update game.c
    now you actually can move!

--- src/game.c
+++ src/game.c
@@ -5,32 +5,45 @@
 
 vec3 stick = {0,0,0}; //joystick x y
 
-vec3 pos = {0,0,-6};
+vec3 pos = {0,0,-5};
 vec3 rot = {0,0,0};
 
-GLfloat speed = 2.0;
+GLfloat speed = 0.2;
 
+float toDeg(float r)
+{
+	return r*180.f/M_PI;
+}
 void renderShip()
 {
-	glRotatef(rot.x,1,0,0);
-	glRotatef(rot.y,0,1,0);
-	glRotatef(rot.z,0,0,1);
-	glTranslatef(-pos.x,-pos.y,-pos.z);
+	glRotatef(toDeg(rot.x),1,0,0);
+	glRotatef(toDeg(rot.y),0,1,0);
+	glRotatef(toDeg(rot.z),0,0,1);
+	glTranslatef(pos.x,pos.y,pos.z);
 	glBegin(GL_TRIANGLES);
 	{
 		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
 		glVertex3f(0.0f, 0.0f, 0.0f);
-		glVertex3f(0.5f, 0.0f, -0.5f);
-		glVertex3f(0.0f, 0.0f, 2.0f);
+		glVertex3f(1.5f, 0.0f, -1.5f);
+		glVertex3f(0.0f, 0.0f, 3.0f);
 		
 		glVertex3f(0.0f, 0.0f, 0.0f);
-		glVertex3f(-0.5f, 0.0f, -0.5f);
-		glVertex3f(0.0f, 0.0f, 2.0f);
+		glVertex3f(-1.5f, 0.0f, -1.5f);
+		glVertex3f(0.0f, 0.0f, 3.0f);
 
-		glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
+		glColor4f(1.0f, 2.0f, 0.0f, 1.0f);
 		glVertex3f(0.0f, 0.0f, 0.0f);
+		glVertex3f(0.0f, 2.0f, -1.0f);
+		glVertex3f(0.0f, 0.0f, 3.0f);
+
+		glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
 		glVertex3f(0.0f, 1.0f, -0.5f);
 		glVertex3f(0.0f, 0.0f, 2.0f);
+		glVertex3f(0.5f, 0.0f, -0.5f);
+
+		glVertex3f(0.0f, 1.0f, -0.5f);
+		glVertex3f(0.0f, 0.0f, 2.0f);
+		glVertex3f(-0.5f, 0.0f, -0.5f);
 	}
 	glEnd();
 }
@@ -53,17 +66,24 @@ void render()
 }
 void update(double delta)
 {
-	rot.y += 10*delta*stick.y;
-	rot.x += 10*delta*stick.x;
+	rot.z += delta*stick.x/10;
+	rot.x += delta*stick.y/10;
+	stick.x = stick.y = 0;
+	printf("%f %f\n",toDeg(rot.z),toDeg(rot.x));
 	speed += delta*stick.z;
 	if(speed)//used for movement
 	{
-		//vec3 dVec = vec3_rot(_vec3(0.f,0.f,speed*delta),rot);
-		vec3 dVec = _vec3(speed*delta*sinf(rot.x)*cosf(rot.y),speed*delta*sinf(rot.x)*sinf(rot.y),speed*delta*cosf(rot.x));		
+		vec3 dVec = vec3_rot(_vec3(0.f,0.f,speed*delta),rot);
 		pos.x += dVec.x;
 		pos.y += dVec.y;
 		pos.z += dVec.z;
 	}
+	while(rot.x < 0) rot.x += 2.f*M_PI;
+	while(rot.x >= 2.f*M_PI) rot.x -= 2.f*M_PI;
+	while(rot.y < 0) rot.y += 2.f*M_PI;
+	while(rot.y >= 2.f*M_PI) rot.y -= 2.f*M_PI;
+	while(rot.z < 0) rot.z += 2.f*M_PI;
+	while(rot.z >= 2.f*M_PI) rot.z -= 2.f*M_PI;
 }
 void handleKey(SDL_Keycode key, Uint32 status)
 {
@@ -79,6 +99,7 @@ void handleKey(SDL_Keycode key, Uint32 status)
 }
 void joystick(double x, double y) //range of [-1,1] for x and y
 {
+	
 	stick.x = x;
 	stick.y = y;
 	//stick.x = atan2(y,x); //polar

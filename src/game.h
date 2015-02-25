b4081515112843bc43d61b798adf43dd562ea3d7
pbandj24 <2015376@sluh.org>
2015-02-24 20:49:09 -0600

    Update game.h

--- src/game.h
+++ src/game.h
@@ -1,11 +1,9 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "SDL2/SDL.h"
-
 void render();
 void update(double delta);
-void handleKey(SDL_Keycode key, Uint32 status);
+void setAcceleration(double d);
 void joystick(double x, double y);
 void init();
 

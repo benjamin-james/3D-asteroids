69ef9dbb7dc5b3c8e66aad8a8588257ad900fb65
pbandj24 <2015376@sluh.org>
2015-02-28 17:28:55 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -17,6 +17,7 @@ int main(int argc, char **argv)
   Uint32 now,last = SDL_GetTicks();
   SDL_ShowCursor(SDL_DISABLE);
   SDL_WarpMouseInWindow(window,width/2,height/2);
+  game_init();
   while(input(window))
   {
   	render();

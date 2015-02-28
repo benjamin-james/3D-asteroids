345526f05c58ceb452c3cc5143b67644d6360ff7
pbandj24 <2015376@sluh.org>
2015-02-28 17:11:16 -0600

    Update entity.c

--- src/entity.c
+++ src/entity.c
@@ -9,7 +9,8 @@ entity entity_create()
   entity e = malloc(sizeof(entity_t));
   e->pos = _vec3(0,0,0);
   e->rot = _vec3(0,0,0);
-  e->vel = _vec3(0,0,0);
+  e->lin = _vec3(0,0,0);
+  e->ang = _vec3(0,0,0);
   e->index = 0;
   return e;
 }
@@ -27,7 +28,10 @@ void entity_render(entity e)
 void entity_update(entity e, float delta)
 {
 	if(!e) return;
-	vec3 dVec = vec3_rot(e->vel,e->rot);
+	e->rot.x += e->ang.x*delta;
+	e->rot.y += e->ang.y*delta;
+	e->rot.z += e->ang.z*delta;
+	vec3 dVec = vec3_rot(e->lin,e->rot);
 	e->pos.x += dVec.x*delta;
 	e->pos.y += dVec.y*delta;
 	e->pos.z += dVec.z*delta;

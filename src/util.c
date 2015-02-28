13f7d2f2124caf4179afb7201ddbf28ae537a24d
pbandj24 <2015376@sluh.org>
2015-02-28 14:49:38 -0600

    Update and rename src to src/util.c

---
+++ src/util.c
@@ -0,0 +1,29 @@
+#include "stdlib.h"
+#include "math.h"
+#include "SDL2/SDL.h"
+#include "entity.h"
+#include "util.h"
+
+void showFatalError(char *msg)
+{
+	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,"Fatal Error!",msg,NULL);
+	SDL_Quit();
+	exit(-1);
+}
+void showWarning(char *msg)
+{
+	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_WARNING,"Warning!",msg,NULL);
+}
+inline float toDeg(float r)
+{
+	return r*180.f/M_PI;
+}
+inline float toRad(float r)
+{
+	return r*M_PI/180.f;
+}
+entity loadObj()
+{
+	entity e = NULL;
+	return e;
+}

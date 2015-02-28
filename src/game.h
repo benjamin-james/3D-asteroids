256614026cab6a6f70947725bb5cd0eb36a606f7
pbandj24 <2015376@sluh.org>
2015-02-28 14:47:21 -0600

    Update game.h

--- src/game.h
+++ src/game.h
@@ -1,12 +1,7 @@
 #ifndef GAME_H
 #define GAME_H
 
-#define DEBUG 1 //for now
-
+void update(double);
 void render();
-void update(double delta);
-void setAcceleration(double d);
-void joystick(double x, double y);
-void init();
 
 #endif

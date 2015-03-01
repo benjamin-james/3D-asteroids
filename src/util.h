c8f68a4e6f227a595de733b125f83f997c9ef0fe
pbandj24 <2015376@sluh.org>
2015-03-01 01:09:21 -0600

    Update util.h

--- src/util.h
+++ src/util.h
@@ -3,11 +3,12 @@
 
 #define DEBUG 1 //for now
 #include "entity.h"
+#include "SDL2/SDL_opengl.h"
 
 void showFatalError(char *);
 void showWarning(char *); 
 inline float toDeg(float);
 inline float toRad(float);
 entity loadObj(char *);
-
+GLuint loadBMP(char *);
 #endif

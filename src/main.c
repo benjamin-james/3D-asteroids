b8d2b704ce05d3eaaa990c68ed90bb8c4d4c9c15
pbandj24 <2015376@sluh.org>
2015-03-02 10:13:47 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,11 +3,15 @@
 #include "game_gl.h"
 #include "events.h"
 #include "util.h"
+#include "signal.h"
 
 int main(int argc, char **argv)
 {
   int width = 800;
   int height = 600;
+  signal(SIGSEGV,sighandler);
+  signal(SIGINT,sighandler);
+  signal(SIGFPE,sighandler);
   SDL_Init(SDL_INIT_EVERYTHING);
   SDL_Window *window = SDL_CreateWindow(*argv, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);	
 	if(!window) showFatalError("Could not create window");

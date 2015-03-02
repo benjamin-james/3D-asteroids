01b2f8d36fec9e8c291377f233fbcc74a10b3a8b
pbandj24 <2015376@sluh.org>
2015-03-02 09:55:51 -0600

    Update util.h

--- src/util.h
+++ src/util.h
@@ -4,11 +4,14 @@
 #define DEBUG 1 //for now
 #include "entity.h"
 #include "SDL2/SDL_opengl.h"
+#include "math.h"
 
+#define toDeg(r) (r*180/M_PI)
+#define toRad(r) (r*M_PI/180)
 void showFatalError(char *);
 void showWarning(char *); 
-inline float toDeg(float);
-inline float toRad(float);
+//inline float toDeg(float);
+//inline float toRad(float);
 entity loadObj(char *);
 char *loadFile(char *);
 GLuint loadBMP(char *);
